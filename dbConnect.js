8a8d3f7be116ded95ed0ce62ae9a268656e492de
KONG <kongwe@deakin.edu.au>
2022-05-19 10:17:26 +1000

    update ev_Station.json

--- dbConnect.js
+++ dbConnect.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const MongoClient = require("mongodb").MongoClient;
 
-const uri = "mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PASSWORD+"@cluster0.ycgjk.mongodb.net/SIT725_2022_t1?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:motdepasse@cluster0.ycgjk.mongodb.net/SIT725_2022_t1?retryWrites=true&w=majority";
 const client = new MongoClient(uri,{ useNewUrlParser: true })
 
 client.connect((err,db) => {
